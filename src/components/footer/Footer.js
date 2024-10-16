8596465dbe061a02cd014161fe44f8c18c02137d
Pujarani06052001 <ranipooja21@navgurukul.org>
2024-10-16 23:34:30 +0530

    E-Commerce

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Footer() {
+    return (
+        <footer className='bg-black text-white'>
+                <div className='flex justify-center flex-col items-center'>
+                    <p className='text-center'>© Copyright 2024 React Shop | All Rights Reserved</p>
+               
+            </div>
+        </footer>
+    );
+}
